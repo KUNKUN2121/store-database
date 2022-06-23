acbac90f2abe553bf41f147a23907bb49e96d467
KUNKUN2121 <kunkun7532@gmail.com>
2022-06-23 02:19:59 +0000

    個数表示

--- api/productName.php
+++ api/productName.php
@@ -64,12 +64,40 @@ $img = 'https://store-project.f5.si/img/';
         }
 
 
+        // 個数 取得
+        try{
+            //$barcode = 4549131970258;
+            $stmt = $pdo->prepare('SELECT quantity FROM product_contents WHERE barnum = :barnum');
+            $stmt->bindValue(':barnum', $barcode);
+            //実行
+            $res = $stmt->execute();
+
+            // データを取得
+            if( $res ) {
+                $data = $stmt->fetch();
+                //表示処理
+                if($data[0] != null){
+                    $quantity  = $data[0];
+                    $return = 0; // 完了通知
+                }else{
+                    //登録されてない
+                    $itemname = 'error';
+                    $return = 1; // 処理終了
+                }
+            }
+        } catch (PDOException $e) {
+            // 接続できなかったらエラー表示
+            echo 'error: ' . $e->getMessage();
+        }
+
+
 
     // json 変換処理
     if($return == 0){
         $person = [
         'itemname' => $itemname,
         'barcode' => $barcode,
+        'quantity' => $quantity,
         'category' => 'taro@example.com',
         'price' => 18,
         'imgURL' => $imgURL,
