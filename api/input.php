f6764190d14193d9f9c71cdbe145764c203bb3b5
KUNKUN2121 <kunkun7532@gmail.com>
2022-06-23 01:08:13 +0000

    json add database

--- api/input.php
+++ api/input.php
@@ -1,17 +1,4 @@
 <?php
-    // json受け取り
-    $json = file_get_contents('php://input');
-    $data = json_decode($json, true);
-    //データ代入
-    $barcode = $data['barcode']; 
-    $quantity = $data['quantity'];
-
-
-    //// debug /////
-        // echo($barcode.'<div>');
-        // echo($quantity.'<div>');
-    //// debug ////
-    
     //データベース接続
     try {
         $dsn = 'mysql:dbname=store;host=localhost;charset=utf8mb4';
@@ -21,44 +8,77 @@
         $pdo = new PDO($dsn, $username, $password, $driver_options);
         // 入力した値をデータベースへ登録
         } catch (PDOException $e) {
-        echo 'DB接続エラー！: ' . $e->getMessage();
+        echo 'DB接続エラー!: ' . $e->getMessage();
     }
 
-    try{
-        $stmt = $pdo->prepare('UPDATE product_contents SET quantity = quantity + :quantity WHERE barnum = :barnum');
-        $stmt->bindValue(':barnum', $barcode);
-        $stmt->bindValue(':quantity', $quantity);
-        //実行
-        $res = $stmt->execute();
-        $count=$stmt->rowCount();
+    // json受け取り
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+    //データ代入
+    // var_dump($data);
+    $end = count($data);
+    echo $end;
+    for ($i = 0; $i < $end; $i++) {
+        $barcode = $data[$i][0];
+        $quantity = $data[$i][1];
+        //echo($barcode.' 個数は '.$quantity.'<div>');
 
-        // データを取得
-        if( $res == true) {
-            // 何個変えた？
-            if($count >= 1){
-                echo('変更したよ');
+        try{
+            $stmt = $pdo->prepare('UPDATE product_contents SET quantity = quantity + :quantity WHERE barnum = :barnum');
+            $stmt->bindValue(':barnum', $barcode);
+            $stmt->bindValue(':quantity', $quantity);
+            //実行
+            $res = $stmt->execute();
+            $count=$stmt->rowCount();
+    
+            // データを取得
+            if( $res == true) {
+                // 何個変えた？
+                if($count >= 1){
+                    echo('変更したよ');
+                }else{
+                    echo('変更なし');
+                }
+                // var_dump($res);
+                // $data = $stmt->fetch();
+                //     if($data[0] != null){
+                //         //関数代入
+                //         $now_quantity  = $data[0];
+                //         $return = 0; // 完了通知
+                //         echo ($now_quantity);
+                //     }else{
+                //         //登録されてない
+                //         $itemname = 'error';
+                //         $return = 1; // 処理終了
+                //     }
             }else{
-                echo('変更なし');
+                echo('resあるけど countない');
             }
-            // var_dump($res);
-            // $data = $stmt->fetch();
-            //     if($data[0] != null){
-            //         //関数代入
-            //         $now_quantity  = $data[0];
-            //         $return = 0; // 完了通知
-            //         echo ($now_quantity);
-            //     }else{
-            //         //登録されてない
-            //         $itemname = 'error';
-            //         $return = 1; // 処理終了
-            //     }
-        }else{
-            echo('resあるけど countない');
+    
+        } catch (PDOException $e) {
+            echo('データベースエラー<div>');
+            // 接続できなかったらエラー表示
+            echo 'error: ' . $e->getMessage();
         }
 
-    } catch (PDOException $e) {
-        echo('データベースエラー<div>');
-        // 接続できなかったらエラー表示
-        echo 'error: ' . $e->getMessage();
     }
+
+    // foreach($data as $i){
+    //     $barcode = $data[$i][0];
+    //     $quantity = $data[$i][1];
+    //     echo($barcode.' 個数は '.$quantity.'<div>');
+    // }
+
+    
+
+
+    //// debug /////
+        // echo($barcode.'<div>');
+        // echo($quantity.'<div>');
+    //// debug ////
+    
+
+
+
+
 ?>
