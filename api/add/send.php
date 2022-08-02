2a432096a280f4b7e60506a0d9b50a6da2c89931
KUNKUN2121 <kunkun7532@gmail.com>
2022-08-02 11:23:44 +0000

    comit

--- api/add/send.php
+++ api/add/send.php
@@ -33,7 +33,16 @@
 
 
 <?php
-require_once "../adatabase.php";
+try {
+        $dsn = 'mysql:dbname=store;host=localhost;charset=utf8mb4';
+        $username ='store';
+        $password = 'vGciFPmVGTdd86R682U75MfNdzAQMg';
+    
+        $pdo = new PDO($dsn, $username, $password, $driver_options);
+        // 入力した値をデータベースへ登録
+        } catch (PDOException $e) {
+        echo 'DB接続エラー!: ' . $e->getMessage();
+    }
 echo $_POST["barcode"];
 echo '<div>';
 // echo $_POST["content"];
